f43dae716c628c812487979765b368a868688294
Surabhi Raghavan <surabhiraghavan1@gmail.com>
2024-09-10 01:36:59 -0400

    Adding pseudocode

--- Q1.py
+++ Q1.py
@@ -1,1 +1,18 @@
-dskasdk
+"""
+PSEUDOCODE: 
+Input: A list of integers 
+Output: All subsets of 3 elements
+Variables
+    n: length of list
+    
+
+Function subsetOfThree(input, start, subset):
+    If Length of subset == 3
+        Print subset
+        Return
+    If start >= Length of elements
+        Return
+    For i from start to length of input-1
+        Call subsetOfThree(input, i+1, subset+ [input[i]])
+    
+"""
