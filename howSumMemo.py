1f0f09ed91b5800752a733c46a96bbd642ba364c
Surabhi Raghavan <surabhiraghavan1@gmail.com>
2024-09-25 21:31:33 -0400

    Final Updates to Memoization

--- howSumMemo.py
+++ howSumMemo.py
@@ -1,4 +1,4 @@
-def howSum1(targetSum, numbers, memo=None):
+def howSum_memo(targetSum, numbers, memo=None):
     if memo is None:
         memo = {}
     if targetSum in memo:
@@ -10,12 +10,16 @@ def howSum1(targetSum, numbers, memo=None):
     
     for num in numbers:
         remainder = targetSum - num
-        remainderResult = howSum1(remainder, numbers, memo)
-        
+        remainderResult = howSum_memo(remainder, numbers, memo)
         if remainderResult is not None:
-            memo[targetSum]= remainderResult + [num]
+            memo[targetSum] = remainderResult + [num]
             return memo[targetSum]
+    
     memo[targetSum] = None
     return None
 
 
+if __name__ == "__main__":
+    targetSum= int(input("Enter the Target Sum\n"))
+    numbers = list(map(int, input("Enter numbers in the array with a space inbetween\n").split()))
+    print(howSum_memo(targetSum, numbers))
