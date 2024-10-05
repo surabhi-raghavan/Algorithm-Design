900e6cd0dabaa0feb224ebf9acf5e6e8acf31445
Surabhi Raghavan <surabhiraghavan1@gmail.com>
2024-10-05 18:02:30 -0400

    Proj 1 Question 1

---
+++ Proj1Array.py
@@ -0,0 +1,6 @@
+"""
+Design and implement your own algorithm that takes the array A with size m+n as input where:
+Subarray A[1], A[2],...A[m] sorted in ascending order
+Subarray A[m+1], A[m+2],...A[n] sorted in ascending order
+and merges the two subarrays using an auxiliary array Aux of size min {m, n} back into array A sorted in ascending order. You must design and implement your own sorting function. Use of sorting functions in libraries is not permitted. 
+"""
