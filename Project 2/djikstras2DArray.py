c346427dab5104ec8de335061d319eab7364fbec
Surabhi Raghavan <surabhiraghavan1@gmail.com>
2024-10-20 02:33:01 -0400

    define class matrix to represent a 2D array

--- Project 2/djikstras2DArray.py
+++ Project 2/djikstras2DArray.py
@@ -3,3 +3,16 @@ import time
 
 INF = float('inf')
 MAX_NODES = 2000
+
+class Matrix:
+    def set(self, i, j, value):
+        row = list(self.data[i])
+        row[j] = value
+        self.data = self.data [:i] + (tuple (row), ) +self.data [i+1 :]
+        
+    def get(self, i, j):
+        return self.data[i][j]
+    
+    def djikstra(graph, src, num_nodes):
+        
+        
